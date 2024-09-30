584dbdca318e30075576777a5a2688204bba3eec
A. Orr <andrewcjorr@gmail.com>
2024-09-30 19:13:02 +0100

    simplify frame counting

--- smooth_video_to_single_frame.m
+++ smooth_video_to_single_frame.m
@@ -4,25 +4,20 @@
 filename = "example/example.mp4";
 filename_char = convertStringsToChars(filename); %convert it to characters
 
-
+%extra decimation
+decimation_factor = 100; %skip frames to speed up video - use with caution
 
 outputname = filename_char(1:end-4) + "_out.mp4";
 output_image_name_base = filename_char(1:end-4) + "_frame_";
 v = VideoReader(filename); %create videoreader object to read in frames from selected video
 
-%the filename of the chd header needs to have the dot removed and then be
-%appended with .chd.
-chd_filename = regexprep(filename,'[.]','') + '.chd';
 
-%import the video's metadata (FPS, exposure, etc) using my custom read_chd func
-C = read_chd(chd_filename);
 
 numOfFramesUsed = 1;
 
 
-for fi = 1:100:(C.ImageCount)
+for fi = 1:decimation_factor:(v.NumFrames)
     frame = im2double(read(v,fi));
-    [rows columns numberOfColorBands] = size(frame);
     if fi == 1
         sumImage = frame;
     else
